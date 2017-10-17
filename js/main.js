df1d597bcb12aa5d1e957df5d5b323a5653ead18
Dharma Sanjyal <rajsan44@gmail.com>
2017-10-17 12:09:54 -0700

    second commit

--- js/main.js
+++ js/main.js
@@ -7,57 +7,57 @@ topic.textContent = "Your Daily Horoscope!";
 var zodiac = [
  {
     sign: "aries",
-   day: "You will meet special people today",
+   day: "well spent by meeting with special people today.",
    image: "img/aries.png"
  },
  {
  	sign: "aquarius",
- 	day: "full of adventures",
+ 	day: "full of adventures.",
  	image: "img/aquarius.png"
  },
  {
  	sign: "cancer",
- 	day: "Wont be that good but still there is hope",
+ 	day: "kind of rough but still there is hope",
  	image: "img/cancer.png"
  },
  {
  	sign: "leo",
- 	day: "full of positive vibes",
+ 	day: "full of positive vibes.",
  	image: "img/leo.png"
  },
  {
  	sign: "capricorn",
- 	day: "mindgames and trouble",
+ 	day: " spent hovering around mindgames and full of trouble.",
  	image: "img/capricon.png"
  },
  {
  	sign: "pisces",
- 	day: "able to handle anything coming on the way",
+ 	day: "full of courage and you will able to handle anything coming on the way",
  	image: "img/pisces.png"
  },
  {
    sign: "libra",
-   day: "Unhindered motion and fluctuation",	
+   day: "full of motion and fluctuation",	
    image: "img/libra.png"
  },
  {
  	sign: "sagittarius", 
- 	day: "Experimental day",
+ 	day: "Experimental and you can be a part of something new.",
  	image: "img/sagittarius.png"
  },
  {
    sign: "taurus",
-   day: "very practical day, have to analyze",
+   day: "very practical.",
    image: "img/taurus.png"	
  },
  {
  	sign: "virgo",
- 	day: "secure and self-willed day with good fortune",
+ 	day: "secure and self-willed with good fortune.",
  	image: "img/virgo.png"
  },
  {
  	sign: "gemini",
- 	day: "full of quality time with relatives",
+ 	day: "full of quality time with relatives.",
  	image: "img/gemini.png"
  },
  {
@@ -74,7 +74,7 @@ function horoscope() {
 	for(var i = 0; i < zodiac.length; i = i + 1) {
 console.log("users value lowercase is: " + userInput.toLowerCase());
 		console.log("current zodiac sign in loop is: " + zodiac[i].sign);
-	if(userdata.value.toLowerCase() === zodiac[i].sign) {
+	if(userdata.value.toLowerCase() == zodiac[i].sign) {
 			// to confirm the if statement ran
 			console.log("if statement ran");
 
@@ -91,15 +91,16 @@ console.log("users value lowercase is: " + userInput.toLowerCase());
 			document.getElementById("day").textContent = "Your day will be " + zodiac[i].day;
 
 			// stop the function because we found a match and added the data to the screen!!!
-			return;
+			return(horoscope);
 		};
 
 		// confirm no matches are found if that's the case
 		console.log("no matches were found, user failed to type in a correct zodiac sign");
 
 		// if no match is found, do opposite of above, mostly clearing content and images
-		document.getElementById("sign").textContent = "Not one of the signs. Try again!";
+		document.getElementById("sign").textContent = "Not one of the zodiac. Try again!";
 		document.getElementById("day").textContent = "";
 		document.getElementById("image").src = "";
 	};
+	userInput.reset()
 };
